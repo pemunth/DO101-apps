a22513dbc02d7950b079fd2f42bb359b8945adff
pemunth <peter.munthe@atea.se>
2020-05-26 09:54:06 +0200

    aa

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello Mars!\n')
+  res.send('Hello Mars! New info\n')
 });
 
 app.listen(8080, function () {
